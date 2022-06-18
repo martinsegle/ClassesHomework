1dcccab9f60b2a240b19391754f0269545a3ecfd
martinsegle <mr.martins.egle@gmail.com>
2022-06-18 10:08:16 +0300

    Add project files.

---
+++ ClassesHomework/Program.cs
@@ -0,0 +1,38 @@
+﻿using ClassesHomework;
+
+Console.WriteLine("Ievadi sava telefona marku!");
+string telephoneBrand = Console.ReadLine();
+
+Console.WriteLine("Ievadi sava telefona modeli!");
+string telephoneModel = Console.ReadLine();
+
+Console.WriteLine("Ievadi sava telefona garumu!");
+string telephoneLenghtText = Console.ReadLine();
+int telephoneLenght = int.Parse(telephoneLenghtText);
+
+Console.WriteLine("Ievadi sava telefona platumu!");
+string telephoneWidthText = Console.ReadLine();
+int telephoneWidth = int.Parse(telephoneWidthText);
+
+Console.WriteLine("Ievadi sava telefona augstumu!");
+string telephoneHightText = Console.ReadLine();
+int telephoneHight = int.Parse(telephoneHightText);
+
+Telephone telephone = new Telephone();
+
+telephone.TelephoneBrand = telephoneBrand;
+telephone.TelephoneModel = telephoneModel;
+telephone.TelephoneLenght = telephoneLenght;
+telephone.TelephoneWidth = telephoneWidth;
+telephone.TelephoneHight = telephoneHight;
+
+telephone.Call("12345678");
+
+telephone.SendSMS("12345678");
+
+Console.WriteLine();
+Console.WriteLine("=============================");
+Console.WriteLine();
+
+
+
---
+++ ClassesHomework/Telephone.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace ClassesHomework
+{
+    public class Telephone
+    {
+        public string TelephoneBrand { get; set; }
+        public string TelephoneModel { get; set; }
+        public int TelephoneLenght { get; set; }
+        public int TelephoneWidth { get; set; }
+        public int TelephoneHight { get; set; }
+        
+        public void Call(string phoneNumber)
+        {
+            Console.WriteLine($"Call form {TelephoneBrand} {TelephoneModel} to {phoneNumber}");
+
+        }
+
+        public void SendSMS(string phoneNumber)
+        {
+            Console.WriteLine($"Send SMS form {TelephoneBrand} {TelephoneModel} to phone number {phoneNumber}");
+
+        }
+
+
+
+    }
+}
