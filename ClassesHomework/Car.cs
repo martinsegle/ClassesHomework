bc0ef817929f294809df3f00a47549e3047b57da
martinsegle <mr.martins.egle@gmail.com>
2022-06-19 17:22:01 +0300

    2

--- ClassesHomework/Car.cs
+++ ClassesHomework/Car.cs
@@ -11,32 +11,36 @@ namespace ClassesHomework
         public string CarBrand { get; set; }
         public string CarLicensePlate { get; set; }
         public double CarSpeed { get; set; }
-        
+        public bool IsSpeed { get; set; }
         public void StartToDrive()
         {
-            CarSpeed = carSpeed;
-
-
+            Console.WriteLine($"Ātrums ir {CarSpeed}, automašīna ir uzsākusi braukt {IsSpeed}");
         }
 
-        public void IncreaseSpeed()
+        public double IncreaseSpeed()
         {
-
+            CarSpeed += 10;
+            Console.WriteLine($"Automašīna paātrinās, jo ātrums ir {CarSpeed}");
+            return CarSpeed;
         }
 
-        public void DecreaseSpeed()
+        public double DecreaseSpeed()
         {
-
+            CarSpeed -= 10;
+            Console.WriteLine($"Automašīna palēninās, jo ātrums ir {CarSpeed}");
+            return CarSpeed;
         }
 
-        public void StopToDrive()
+        public double StopToDrive()
         {
-
+            CarSpeed = 0;
+            Console.WriteLine($"Automašīna ir apstājusies, jo ātrums ir {CarSpeed}");
+            return CarSpeed;
         }
 
         public void Horn()
         {
-
+            Console.WriteLine($"Beep beep");
         }
     }
 }
--- ClassesHomework/Program.cs
+++ ClassesHomework/Program.cs
@@ -1,42 +1,42 @@
 ﻿using ClassesHomework;
 
-Console.WriteLine("Ievadi sava telefona marku!");
-string telephoneBrand = Console.ReadLine();
+//Console.WriteLine("Ievadi sava telefona marku!");
+//string telephoneBrand = Console.ReadLine();
 
-Console.WriteLine("Ievadi sava telefona modeli!");
-string telephoneModel = Console.ReadLine();
+//Console.WriteLine("Ievadi sava telefona modeli!");
+//string telephoneModel = Console.ReadLine();
 
-Console.WriteLine("Ievadi sava telefona garumu!");
-string telephoneLenghtText = Console.ReadLine();
-double telephoneLenght = double.Parse(telephoneLenghtText);
+//Console.WriteLine("Ievadi sava telefona garumu!");
+//string telephoneLenghtText = Console.ReadLine();
+//double telephoneLenght = double.Parse(telephoneLenghtText);
 
-Console.WriteLine("Ievadi sava telefona platumu!");
-string telephoneWidthText = Console.ReadLine();
-double telephoneWidth = double.Parse(telephoneWidthText);
+//Console.WriteLine("Ievadi sava telefona platumu!");
+//string telephoneWidthText = Console.ReadLine();
+//double telephoneWidth = double.Parse(telephoneWidthText);
 
-Console.WriteLine("Ievadi sava telefona augstumu!");
-string telephoneHightText = Console.ReadLine();
-double telephoneHight = double.Parse(telephoneHightText);
+//Console.WriteLine("Ievadi sava telefona augstumu!");
+//string telephoneHightText = Console.ReadLine();
+//double telephoneHight = double.Parse(telephoneHightText);
 
-Console.WriteLine("Kam Tu vēlies piezvanīt (ievadi tālr.nr.)?");
-string phoneNumber = Console.ReadLine();
+//Console.WriteLine("Kam Tu vēlies piezvanīt (ievadi tālr.nr.)?");
+//string phoneNumber = Console.ReadLine();
 
 
-Telephone telephone = new Telephone(telephoneBrand);
+//Telephone telephone = new Telephone(telephoneBrand);
 
-telephone.TelephoneBrand = telephoneBrand;
-telephone.TelephoneModel = telephoneModel;
-telephone.TelephoneLenght = telephoneLenght;
-telephone.TelephoneWidth = telephoneWidth;
-telephone.TelephoneHight = telephoneHight;
+//telephone.TelephoneBrand = telephoneBrand;
+//telephone.TelephoneModel = telephoneModel;
+//telephone.TelephoneLenght = telephoneLenght;
+//telephone.TelephoneWidth = telephoneWidth;
+//telephone.TelephoneHight = telephoneHight;
 
-telephone.Call(phoneNumber);
+//telephone.Call(phoneNumber);
 
-telephone.SendSMS(phoneNumber);
+//telephone.SendSMS(phoneNumber);
 
-Console.WriteLine();
-Console.WriteLine("=============================");
-Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine("=============================");
+//Console.WriteLine();
 
 Console.WriteLine("Ievadi automašīnas marku!");
 string carBrand = Console.ReadLine();
@@ -48,12 +48,14 @@ Console.WriteLine("Ievadi automašīnas ātrumu!");
 string carSpeedText = Console.ReadLine();
 double carSpeed = double.Parse(carSpeedText);
 
+bool isSpeed = carSpeed > 0;
 
 Car car = new Car();
 
 car.CarBrand = carBrand;
 car.CarLicensePlate = carLicensePlate;
 car.CarSpeed = carSpeed;
+car.IsSpeed = isSpeed;
 
 car.StartToDrive();
 car.IncreaseSpeed();
